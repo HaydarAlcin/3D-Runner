8759895145cd4bfa32dabb45370cf402c405c37f
Haydar Alçin <hydralcin@gmail.com>
2023-06-14 01:31:36 +0300

    Added project
    CheckCollisions script, CamFollowPlayer script, PlayerController script added

---
+++ 3D-Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runningSpeed;
+    public float xSpeed;
+
+    public float limitX;
+
+    private void Update()
+    {
+        SwipeCheck();
+    }
+
+
+    void SwipeCheck()
+    {
+        float newX = 0;
+        float touchXDelta = 0;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        transform.position = newPosition;
+    }
+}
