7ce58f95e2daee80fd9f65cec1cd5cbb2022714b
Haydar Alçin <hydralcin@gmail.com>
2023-06-22 02:26:56 +0300

    Added Scoreboard

--- 3D-Runner/Assets/Scripts/GameManager.cs
+++ 3D-Runner/Assets/Scripts/GameManager.cs
@@ -7,11 +7,15 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
-    public InGameRanking ig;
+    //public static GameManager instance;
+    ////public InGameRanking ig;
 
-    private GameObject[] runner;
-    List<RankingSystem> sortArray = new List<RankingSystem>();
+    //private GameObject[] runner;
+    //List<RankingSystem> sortArray = new List<RankingSystem>();
+
+    public List<Vector3> characterPositions = new List<Vector3>();
+    public GameObject[] Runners,ScoreBoard;
+    public Transform target;
 
     public TMP_InputField inputPlayerName;
     public TMP_Text Player;
@@ -21,6 +25,13 @@ public class GameManager : MonoBehaviour
         //instance = this;
         //runner = GameObject.FindGameObjectsWithTag("Runner");
         //ig = FindObjectOfType<InGameRanking>();
+
+        characterPositions.Add(Runners[0].transform.position);
+        characterPositions.Add(Runners[1].transform.position);
+        characterPositions.Add(Runners[2].transform.position);
+        characterPositions.Add(Runners[3].transform.position);
+        characterPositions.Add(Runners[4].transform.position);
+
         Time.timeScale = 0f;
     }
 
@@ -35,7 +46,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        //CalculateRanking();
+        CalculateRanking();
     }
 
     public void StartButton()
@@ -51,18 +62,26 @@ public class GameManager : MonoBehaviour
 
     void CalculateRanking()
     {
-        sortArray = sortArray.OrderBy(x => x.distance).ToList();
+        //sortArray = sortArray.OrderBy(x => x.distance).ToList();
 
-        sortArray[0].rank = 1;
-        sortArray[1].rank = 2;
-        sortArray[2].rank = 3;
-        sortArray[3].rank = 4;
-        sortArray[4].rank = 5;
+        //sortArray[0].rank = 1;
+        //sortArray[1].rank = 2;
+        //sortArray[2].rank = 3;
+        //sortArray[3].rank = 4;
+        //sortArray[4].rank = 5;
 
-        ig.a = sortArray[4].name;
-        ig.b = sortArray[3].name;
-        ig.c = sortArray[2].name;
-        ig.d = sortArray[1].name;
-        ig.e = sortArray[0].name;
+        //ig.a = sortArray[4].name;
+        //ig.b = sortArray[3].name;
+        //ig.c = sortArray[2].name;
+        //ig.d = sortArray[1].name;
+        //ig.e = sortArray[0].name;
+
+        var sortedCharacters = Runners.OrderBy(character => Vector3.Distance(character.transform.position, target.position)).ToList();
+
+        // Sýralama sonuçlarýný text nesnelerine yazdýr
+        for (int i = 0; i < sortedCharacters.Count; i++)
+        {    
+            ScoreBoard[i].GetComponent<TextMeshProUGUI>().text = $"{i + 1}. {sortedCharacters[i].name}";
+        }
     }
 }
